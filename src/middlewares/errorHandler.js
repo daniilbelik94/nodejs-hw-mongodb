e4ffa3ae8ccd75c1ee896defc9f5c6a45b2d322c
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2025-03-14 11:02:58 +0100

    Fix errorHandler to handle http-errors status codes correctly

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,14 @@
 export default function errorHandler(err, req, res, next) {
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
+  const status = err.status || err.statusCode || 500; 
+  const message = status === 500 ? 'Something went wrong' : err.message; // Якщо статус 500, повідомлення про помилку буде "Something went wrong"
+
+    console.error(`Error occurred: ${err.message}, Status: ${status}`); // Виводимо повідомлення про помилку в консоль
+    
+
+ // Відправляємо відповідь клієнту
+  res.status(status).json({
+    status,
+    message,
     data: err.message,
   });
 }
