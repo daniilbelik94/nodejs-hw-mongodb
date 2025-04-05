2b6bdcb8f6ee017ba5ae3db0cc678a37e18adb0f
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2025-04-05 13:17:05 +0200

    Update contactSchema.js

--- src/schemas/contactSchema.js
+++ src/schemas/contactSchema.js
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(3).max(30).required(),
   phoneNumber: Joi.string().min(3).max(20).required(), 
-  email: Joi.string().email().min(3).max(20).required(),
+  email: Joi.string().email().min(3).max(30).required(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(), 
   isFavourite: Joi.boolean().default(false),
 });
@@ -11,7 +11,7 @@ const contactSchema = Joi.object({
 const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().email().min(3).max(20),
+  email: Joi.string().email().min(3).max(30),
   contactType: Joi.string().valid('work', 'home', 'personal'),
   isFavourite: Joi.boolean(),
 }).or('name', 'phoneNumber', 'email', 'contactType', 'isFavourite');
